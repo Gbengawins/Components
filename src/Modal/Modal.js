6a9a50d36cc480cf019976dd8ea9575b0636f669
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-11-04 20:18:46 +0100

    project setup

---
+++ src/Modal/Modal.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div className='modal'>
+          <div className='modal-content'>
+              <div className='modal-header'>
+                  <h4 className='modal-title'>Modal title</h4>
+              </div>
+              <div className='modal-body'>
+                  <h6>This is modal body</h6>
+              </div>
+        </div>
+          
+    </div>
+  )
+}
+
+export default Modal
